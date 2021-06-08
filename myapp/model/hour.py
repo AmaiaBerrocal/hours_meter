94adfdfc006e012a6e59b4a722cb92115a7b37bb
Amaia Berrocal <amaia.berrocal@gmail.com>
2021-06-08 14:17:35 +0200

    creada clase Hour

---
+++ myapp/model/hour.py
@@ -0,0 +1,11 @@
+class Hour:
+    def __init__(self, id_hours, id_user, year, month, day, hours, minutes, creation_date, modification_date):
+        self.id_hours = id_hours
+        self.id_user = id_user
+        self.year = year
+        self.month = month
+        self.day = day
+        self.hours = hours
+        self.minutes = minutes
+        self.creation_date = creation_date
+        self.modification_date = modification_date
